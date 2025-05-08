4670940f0fe24106c90e3768d1855be1dba25c0c
wooyong8969 <wooyongmeock@gmail.com>
2025-05-08 19:48:13 +0900

    SVM 모델 추가

--- current_experiments/CODE/2_featuring/main.py
+++ current_experiments/CODE/2_featuring/main.py
@@ -1,15 +1,22 @@
 from eeg_dataset import EEGDataset
 from feature_extractor import DWTFeatureExtractor
 import numpy as np
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.svm import SVC
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.pipeline import make_pipeline
 
 mat_path = r'D:\W00Y0NG\PRGM2\2025BCI\current_experiments\DATA\processed\experiment_001_cleaned.mat'
 label_csv_path = r'D:\W00Y0NG\PRGM2\2025BCI\current_experiments\DATA\processed\experiment_001_labels.csv'
 
+# ---------- 1.  전처리 된 데이터셋 불러오기 ---------- #
 dataset = EEGDataset(mat_path, label_csv_path)
 dataset.remove_break()
 
 eeg, labels, fs = dataset.get_data()
 
+
+# ---------- 2. 특징 추출하기 ---------- #
 extractor = DWTFeatureExtractor(wavelet='coif1', level=5)
 
 time_features, freq_features = extractor.extract(eeg)
@@ -26,3 +33,20 @@ features = features.reshape(n_epochs, -1)
 
 print("최종 feature shape:", features.shape)
 print("최종 라벨 개수:", len(labels))
+
+# ---------- 3. 모델 학습 ---------- #
+
+le = LabelEncoder()
+y = le.fit_transform(labels)
+
+X_train, X_test, y_train, y_test = train_test_split(features, y, test_size=0.2, stratify=y, random_state=42)
+
+clf = make_pipeline(StandardScaler(), SVC(kernel='rbf', C=1, gamma='scale'))
+
+clf.fit(X_train, y_train)
+
+train_acc = clf.score(X_train, y_train)
+test_acc = clf.score(X_test, y_test)
+
+print(f"훈련 정확도: {train_acc:.4f}")
+print(f"학습 정확도:  {test_acc:.4f}")

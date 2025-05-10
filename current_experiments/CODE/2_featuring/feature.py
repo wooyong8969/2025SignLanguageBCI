c6a4eef49719cca480cf0c6e68e57194736dd26d
wooyong8969 <wooyongmeock@gmail.com>
2025-05-10 20:05:02 +0900

    데이터 증강

--- current_experiments/CODE/2_featuring/feature.py
+++ current_experiments/CODE/2_featuring/feature.py
@@ -5,16 +5,17 @@ from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_selection import SelectFromModel
+from eeg_augmenter import EEGAugmenter
 import os
 
 mat_path = r'current_experiments\DATA\processed\experiment_001_cleaned.mat'
 label_csv_path = r'current_experiments\DATA\processed\experiment_001_labels.csv'
 
-features_path = r'current_experiments\DATA\processed\experiment_001_processed_features.npy'
-labels_path = r'current_experiments\DATA\processed\experiment_001_encoded_labels.npy'
+features_path = r'current_experiments\DATA\processed\experiment_001_augmented_features.npy'
+labels_path = r'current_experiments\DATA\processed\experiment_001_augmented_labels.npy'
 
 
-# ---------- 1.  전처리 된 데이터셋 불러오기 ---------- #
+# ---------- 1. 전처리 된 데이터셋 불러오기 ---------- #
 
 dataset = EEGDataset(mat_path, label_csv_path)
 dataset.remove_break()
@@ -24,6 +25,15 @@ le = LabelEncoder()
 encoded_labels = le.fit_transform(labels)  # y가 ['left', 'right', 'left', ...] 등이라면 0, 1로 변환됨
 
 
+# ---------- 2. 데이터 증강하기 ---------- #
+
+augmenter = EEGAugmenter(noise_level=0.01, max_shift=8)
+aug_eeg, aug_labels = augmenter.augment(eeg, encoded_labels, num_augments=10)
+
+print("증강 후 데이터 shape:", aug_eeg.shape)
+print("증강 후 라벨 수:", len(aug_labels))
+
+
 # ---------- 2. 특징 추출하기 ---------- #
 
 if os.path.exists(features_path) and os.path.exists(labels_path):
@@ -31,16 +41,16 @@ if os.path.exists(features_path) and os.path.exists(labels_path):
 else:
     print("특징 추출 중...")
     extractor = DWTFeatureExtractor(wavelet='coif1', level=5)
-    time_features, freq_features = extractor.extract(eeg)
+    time_features, freq_features = extractor.extract(aug_eeg)
 
     flat_time = extractor.flatten_feature_dict(time_features, extractor.bands)
     flat_freq = extractor.flatten_feature_dict(freq_features, extractor.bands)
-    csp_features = extractor.extract_csp_features(eeg, labels, n_components=4)
-    riemannian_features = extractor.extract_riemannian_features(eeg)
+    csp_features = extractor.extract_csp_features(aug_eeg, aug_labels, n_components=4)
+    riemannian_features = extractor.extract_riemannian_features(aug_eeg)
 
     features = np.concatenate([flat_time, flat_freq, csp_features, riemannian_features], axis=1)
-    n_epochs = eeg.shape[0]
+    n_epochs = aug_eeg.shape[0]
     features = features.reshape(n_epochs, -1)
 
     np.save(features_path, features)
-    np.save(labels_path, encoded_labels)
+    np.save(labels_path, aug_labels)
